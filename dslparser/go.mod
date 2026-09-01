e075f3630e105711fd3936e35c282d721b5fdb38
Roman <50523031+verdoga@users.noreply.github.com>
2026-09-01 18:51:02 +0300

    Delete dslparser directory

--- dslparser/go.mod
+++
@@ -1,3 +0,0 @@
-module dslparser
-
-go 1.26
--- dslparser/internal/structure/types.go
+++
@@ -1,9 +0,0 @@
-package structure
-
-import "dslparser/internal/model"
-
-// Result содержит единую карту блоков и локальные ошибки структурного прохода.
-type Result struct {
-	Blocks      map[int]model.Block
-	Diagnostics map[int][]model.Diagnostic
-}
